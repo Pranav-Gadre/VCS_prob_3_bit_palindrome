b3c063fe436baf1b2efc03611d3c52c4d5412bc0
Cb_cat <leapofleopard@gmail.com>
2024-02-25 11:55:56 +0530

    Commit 1: Provided question details

--- palindrome3b.v
+++ palindrome3b.v
@@ -1,3 +1,32 @@
+/*
+	Details:
+	
+	You are tasked to design a circuit which would 
+	detect a 3-bit palindrome sequence from incoming stream of bits.
+
+	Palindrome code is a sequence of characters which reads the same backward as forward. 
+	For example, the following are palindromes: 101, 010, 111, 000, etc.
+	
+	All the flops should be positive edge triggered with asynchronous resets (if any).
+	
+	Interface Definition
+	x_i : Input stream of bits to the circuit
+	
+	palindrome_o : Output to signal that the current bit and the last two bits together form a
+				palindrome
+	
+	Interface Requirements
+	
+	Output must be given every cycle
+	Input will be a stream of bits presented to the circuit on every cycle
+	
+*/
+
+
+
+
+
+
 module palindrome3b (
   input   wire        clk,
   input   wire        reset,
