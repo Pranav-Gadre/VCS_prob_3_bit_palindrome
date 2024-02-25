8814bfdc55fffd912eb8cee417b4cc4f55e2b991
Cb_cat <leapofleopard@gmail.com>
2024-02-25 11:52:33 +0530

    Commit 0: Created source file.

---
+++ palindrome3b.v
@@ -0,0 +1,12 @@
+module palindrome3b (
+  input   wire        clk,
+  input   wire        reset,
+
+  input   wire        x_i,
+
+  output  wire        palindrome_o
+);
+
+  // Write your logic here...
+
+endmodule
